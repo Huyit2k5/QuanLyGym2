aade3ceb357e0c0f956bcad924a9b8a7ae3eae07
huy hoc code <huyphamit114@gmail.com>
2025-11-11 15:09:05 +0700

    Login

--- QuanLyGym/Forms/frmLogin.Designer.cs
+++ QuanLyGym/Forms/frmLogin.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbo_Server = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(138, 207);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(288, 26);
+            this.txt_UserName.TabIndex = 3;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(141, 286);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(289, 26);
+            this.txt_Password.TabIndex = 4;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.Location = new System.Drawing.Point(142, 347);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(289, 53);
+            this.btn_Login.TabIndex = 6;
+            this.btn_Login.Text = "Đăng nhập";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 40);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Server";
+            // 
+            // cbo_Server
+            // 
+            this.cbo_Server.FormattingEnabled = true;
+            this.cbo_Server.Location = new System.Drawing.Point(141, 122);
+            this.cbo_Server.Name = "cbo_Server";
+            this.cbo_Server.Size = new System.Drawing.Size(285, 28);
+            this.cbo_Server.TabIndex = 9;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(599, 423);
+            this.Controls.Add(this.cbo_Server);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "frmLogin";
             this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbo_Server;
     }
 }
--- QuanLyGym/Forms/frmLogin.cs
+++ QuanLyGym/Forms/frmLogin.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Sql;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,112 @@ namespace QuanLyGym.Forms
         public frmLogin()
         {
             InitializeComponent();
+            this.Load += FrmLogin_Load;
         }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                cbo_Server.Items.Clear();
+
+                DataTable table = SqlDataSourceEnumerator.Instance.GetDataSources();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    string serverName = row["ServerName"].ToString();
+                    string instanceName = row["InstanceName"].ToString();
+
+                    string fullName = string.IsNullOrEmpty(instanceName)
+                        ? serverName
+                        : $"{serverName}\\{instanceName}";
+
+                    // tránh duplicate
+                    if (!cbo_Server.Items.Contains(fullName))
+                        cbo_Server.Items.Add(fullName);
+                }
+
+                // Nếu không tìm thấy gì, thêm fallback cho local SQLEXPRESS
+                if (cbo_Server.Items.Count == 0)
+                {
+                    string localExpress = Environment.MachineName + @"\SQLEXPRESS";
+                    cbo_Server.Items.Add(localExpress);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi liệt kê SQL Server instances:\n" + ex.Message);
+                // thêm fallback chổ này nếu cần
+                cbo_Server.Items.Add(Environment.MachineName + @"\SQLEXPRESS");
+            }
+        }
+
+        //try
+        //{
+        //    RegistryKey rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
+        //    String[] instances = (String[])rk.GetValue("InstalledInstances");
+        //    if (instances.Length > 0)
+        //    {
+        //        foreach (String element in instances)
+        //        {
+        //            if (element == "MSSQLSERVER")
+        //            {
+        //                cbo_Server.Items.Add(Environment.MachineName);
+        //            }
+        //            else
+        //            {
+        //                cbo_Server.Items.Add(Environment.MachineName + @"\" + element);
+        //            }
+        //        }
+        //        cbo_Server.SelectedIndex = 0;
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    MessageBox.Show("Lỗi lấy tên server: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //}
+        //try
+        //{
+        //    cbo_Server.Items.Clear();
+
+        //    RegistryKey rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server");
+        //    if (rk == null)
+        //    {
+        //        // Nếu không tìm thấy, thử thêm nhánh 64-bit
+        //        rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Microsoft\Microsoft SQL Server");
+        //    }
+
+        //    if (rk != null)
+        //    {
+        //        string[] instances = (string[])rk.GetValue("InstalledInstances");
+
+        //        if (instances != null && instances.Length > 0)
+        //        {
+        //            foreach (string instance in instances)
+        //            {
+        //                string serverName = instance.Equals("MSSQLSERVER", StringComparison.OrdinalIgnoreCase)
+        //                    ? Environment.MachineName
+        //                    : Environment.MachineName + @"\" + instance;
+
+        //                cbo_Server.Items.Add(serverName);
+        //            }
+
+        //            cbo_Server.SelectedIndex = 0;
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Không tìm thấy instance SQL Server nào trên máy.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Không thể truy cập registry để lấy danh sách server.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    MessageBox.Show("Lỗi lấy tên server: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //}
     }
-}
+    }
+
